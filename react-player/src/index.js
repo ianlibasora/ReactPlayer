da777665335d956dc4c304b63e5d451b69fad336
Joseph Libasora <56083316+ianlibasora@users.noreply.github.com>
2021-06-16 14:53:51 +0100

    Added basic nav menu

--- react-player/src/index.js
+++ react-player/src/index.js
@@ -3,12 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ThemeProvider, createMuiTheme} from "@material-ui/core/styles"
+
+const theme = createMuiTheme({
+   palette: {
+      primary: {
+         light: '#5f5f5f',
+         main: '#353535',
+         dark: '#0f0f0f',
+         contrastText: '#ffffff',
+      },
+      secondary: {
+         light: '#90e5ff',
+         main: '#56b3ff',
+         dark: '#0084cb',
+         contrastText: '#000000',
+      },
+   },
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+   <React.StrictMode>
+      <ThemeProvider theme={theme}>
+         <App/>
+      </ThemeProvider>
+   </React.StrictMode>,
+   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
